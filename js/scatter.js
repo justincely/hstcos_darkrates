bfb95db7e027745b246964aed8045f79ae93bb87
Justin Ely <justincely@gmail.com>
2017-05-08 17:25:14 -0400

    some stuff

--- js/scatter.js
+++ js/scatter.js
@@ -1,17 +1,20 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
+var margin = {top: 20, right: 20, bottom: 30, left: 60},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
-    
+
 var x = d3.scaleLinear()
     .range([0, width]);
 
 var y = d3.scaleLinear()
     .range([height, 0]);
 
+var ySol = d3.scaleLinear()
+    .range([height, 0]);
+
 var color = d3.scaleLinear()
   .domain([0, 5e-6])
   .range(['cyan', 'darkred'])
-  
+
 var scattersvg = d3.select("#scatter")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -24,6 +27,9 @@ var xAxis = d3.axisBottom()
 var yAxis = d3.axisLeft()
     .scale(y);
 
+var ySolAxis = d3.axisRight()
+    .scale(ySol);
+
 // Load the orbital info stuff
 d3.json("./js/orbital_info.json", function(error, data) {
   if (error) throw error;
@@ -33,12 +39,18 @@ d3.json("./js/orbital_info.json", function(error, data) {
     //Re-center around the map
     d.date = +d.date
     d.dark = +d.dark
+    d.fsol = +d.fsol
   });
 
+  update(data);
+
+});
+
+function update(data) {
   x.domain(d3.extent(data, function(d) { return +d.date; })).nice();
   y.domain(d3.extent(data, function(d) { return +d.dark; })).nice();
+  ySol.domain(d3.extent(data, function(d) { return +d.fsol; })).nice();
 
-  
   scattersvg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
@@ -56,14 +68,25 @@ d3.json("./js/orbital_info.json", function(error, data) {
       .call(yAxis)
     .append("text")
       .attr("class", "label")
-      .attr("transform", "rotate(-90)")
       .attr("y", 6)
       .attr("dy", ".71em")
       .attr("fill", "black")
       .style("text-anchor", "end")
-      .text("Dark Rate (counts/sec/pixel)")
+      .text("Dark Rate")
 
-  scattersvg.selectAll(".dot")
+  scattersvg.append("g")
+      .attr("class", "y axis")
+      .call(ySolAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("transform", "translate(35, 0)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .attr("fill", "orange")
+      .style("text-anchor", "end")
+      .text("F10.7")
+
+  scattersvg.append("g").selectAll(".dot")
       .data(data)
     .enter().append("circle")
       .attr("class", "dot")
@@ -71,6 +94,14 @@ d3.json("./js/orbital_info.json", function(error, data) {
       .attr("cx", function(d) { return x(+d.date); })
       .attr("cy", function(d) { return y(+d.dark); })
       .style("fill", function(d) { return color(+d.dark); });
+
+    scattersvg.append("g").selectAll("circle")
+          .data(data)
+        .enter().append("circle")
+          .attr("r", 3.5)
+          .attr("cx", function(d) { return x(+d.date); })
+          .attr("cy", function(d) { return ySol(+d.fsol); })
+          .style("fill", "orange");
 
       /*
   var legend = scattersvg.selectAll(".legend")
@@ -93,45 +124,28 @@ d3.json("./js/orbital_info.json", function(error, data) {
       .text(function(d) { return d; });
     */
 
-});
+};
 
-d3.select("#dark-min").on("input", function(d, i) {
-  newVal = +this.value;
-  d3.select("#darkrate-min-value").text(newVal);
+// Select only specific detector data.
+d3.select("#detector").on("click", function(d, i) {
+
+  minDark = +document.getElementById("dark-min").value;
+  maxDark = +document.getElementById("dark-max").value;
+
+  y.domain([minDark, maxDark]).nice();
+
+  newDetector = document.getElementById("detector").value;
+  pointsize = +document.getElementById("pointsize").value;
 
   scattersvg.selectAll("circle").transition()
     .duration(2000)
     .delay(500)
     .attr('r', function(d, i) {
-      if (+d.dark <= newVal) {
-        return 0;
-      }
-      return +document.getElementById("pointsize").value;
-    });
-});
-
-// Sekect only FUVB data.
-d3.select("#FUVB").on("click", function(d, i) {
-  scattersvg.selectAll("circle").transition()
-    .duration(2000)
-    .delay(500)
-    .attr('r', function(d, i) {
-      if (d.detector == "FUVB") {
-        return +document.getElementById("pointsize").value;
+      if (newDetector == "BOTH") {
+        return 3.5;
+      } else if (d.detector == newDetector) {
+        return 3.5;
       }
       return 0
-    });
-});
-
-// Sekect only FUVA data.
-d3.select("#FUVA").on("click", function(d, i) {
-  scattersvg.selectAll("circle").transition()
-    .duration(2000)
-    .delay(500)
-    .attr('r', function(d, i) {
-      if (d.detector == "FUVA") {
-        return +document.getElementById("pointsize").value;
-      }
-      return 0
-    });
-});
+    })
+  });
